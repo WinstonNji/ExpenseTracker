8c5460be4584bc37f269874d8c343e5cc8a1f571
Winston Nji <winston00russel@gmail.com>
2025-02-23 01:34:46 +0200

    Implemented balance operation for total balance, income and expense

--- expenseTracker/src/components/balance.jsx
+++ expenseTracker/src/components/balance.jsx
@@ -1,20 +1,64 @@
+import { useContext } from "react"
+import { TransactionContext } from "./newTransaction"
+import { useState, useEffect } from "react"
+
 function BalanceManager (){
+
+    const {expenseArr, incomeArr}  = useContext (TransactionContext)
+
+    const [incomeTotal, setIncomeTotal] = useState('00')
+    const [expenseTotal, setExpenseTotal] = useState('00')
+    const [totalBalance, setBalance] = useState('00')
+
+    useEffect(() => {
+        setIncomeTotal(
+            incomeArr.reduce((acc,element) => {
+                return acc + element
+            },0)
+        )
+    },[incomeArr])
+
+    useEffect(()=> {
+        setExpenseTotal(
+            expenseArr.reduce((acc,element) => (
+                acc + element
+            ),0)
+        )
+    },[expenseArr])
+
+    useEffect(()=> {
+        setBalance(incomeTotal + expenseTotal)
+    },[incomeTotal, expenseTotal])
+
+    
+
+    function balanceRender(sum){
+        if(sum === 0){
+            return <p className="font-bold text-xl text-gray-500 ">{totalBalance.toLocaleString()}</p>
+        }
+        if(sum > 0){
+            return <p className="font-bold text-xl text-green-500 underline">{totalBalance.toLocaleString()}</p>
+        }else{
+            return <p className="font-bold text-xl text-red-500 underline">{totalBalance.toLocaleString()}</p>
+        }
+    }
+
     return (
         <div className=" min-w-[75%] max-w-[75%]"> 
-            <div className="h-16">
-                <h2 className="text-2xl font-semibold">Your Balance</h2>
-                <p className="font-bold">00.00</p>
+            <div className="h-20">
+                <h2 className="text-3xl font-semibold">Your Balance</h2>
+                {balanceRender(totalBalance)}
             </div>
 
             {/* Container */}
             <div className="bg-white flex rounded-sm">
                 <div className=" w-1/2 border-r-2 border-r-pink-500 text-center h-16 flex flex-col justify-center">
                     <h3 className="font-semibold">Income</h3>
-                    <p>00.00</p>
+                    <p className="font-bold text-green-500">{incomeTotal.toLocaleString()}</p>
                 </div>
                 <div className=" w-1/2 text-center flex flex-col justify-center">
                     <h3 className="font-semibold">Expense</h3>
-                    <p>00.00</p>
+                    <p className="text-red-500 font-bold">{expenseTotal.toLocaleString()}</p>
                 </div>
             </div>
         </div>
